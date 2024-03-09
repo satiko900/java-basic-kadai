9e5bf2617cd9836474e02e27cad0e34b9958bfd1
satiko900 <r.ueno.0706@gmail.com>
2024-03-09 19:50:05 +0900

    kadai_021を追加

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,13 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+	public static void main(String[] args) {
+		
+		Dictionary_Chapter21 dictionarychecker = new Dictionary_Chapter21();
+		
+		String[] words = {"apple", "banana", "grape", "orange"};
+		
+		dictionarychecker.checkwords(words);
+		
+	}
+}
